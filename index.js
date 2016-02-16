68400d952239839a3624c1dec59cd20995708e58
Eziohao <eziohao@gmail.com>
2016-02-16 16:48:12 -0500

    Framework
    Building framework

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+bodyParser=require('body-parser'),
+fs = require('fs'),
+logger=require('morgan'),
+port = process.env.PORT || 8888;
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend:true}));
+app.set("view options", {
+layout: false
+});
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+res.render('public/index.html');
+});
+app.listen(port);
+console.log('Express server running at http://localhost:' + port);
